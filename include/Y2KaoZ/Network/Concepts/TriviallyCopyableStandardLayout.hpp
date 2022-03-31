edf3f3b1a79b4516291f0c7b66580ec5c50efdd2
Carlos Gonzalez Y2KaoZ <y2kaoz@gmail.com>
2022-03-31 17:50:25 -0400

    Fixed a bug in TriviallyCopyableStandardLayout Concepts.

--- include/Y2KaoZ/Network/Concepts/TriviallyCopyableStandardLayout.hpp
+++ include/Y2KaoZ/Network/Concepts/TriviallyCopyableStandardLayout.hpp
@@ -6,15 +6,17 @@
 
 namespace Y2KaoZ::Network::Concepts {
 
-template <typename T>
-concept TriviallyCopyableStandardLayoutType = std::is_standard_layout_v<T> && std::is_trivially_copyable_v<T>;
-
 template <typename C>
 concept TriviallyCopyableStandardLayoutContainer = requires(C c) {
   typename C::value_type;
-  TriviallyCopyableStandardLayoutType<typename C::value_type>;
+  std::is_standard_layout_v<typename C::value_type>;
+  std::is_trivially_copyable_v<typename C::value_type>;
   { c.size() } -> std::convertible_to<std::size_t>;
   { std::data(c) } -> std::contiguous_iterator;
 };
+
+template <typename T>
+concept TriviallyCopyableStandardLayoutType =
+  std::is_standard_layout_v<T> && std::is_trivially_copyable_v<T> && !TriviallyCopyableStandardLayoutContainer<T>;
 
 } // namespace Y2KaoZ::Network::Concepts
