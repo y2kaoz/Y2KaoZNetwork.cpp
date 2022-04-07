b8166e36d9a8b793fd45afdb7bd9057edd8f1971
Carlos Gonzalez Y2KaoZ <y2kaoz@gmail.com>
2022-04-07 17:25:24 -0400

    Endian tests uses REQUIRES now.

--- tests/Y2KaoZ/Network/Buffers/Endian.cpp
+++ tests/Y2KaoZ/Network/Buffers/Endian.cpp
@@ -19,83 +19,83 @@ TEST_CASE("Native to Endian") { // NOLINT
   constexpr std::uint64_t inv64 = 0x0100000000000000;
 
   SECTION("toEndian") {
-    CHECK(toEndian<std::endian::native>(one8) == one8);
-    CHECK(toEndian<std::endian::native>(one16) == one16);
-    CHECK(toEndian<std::endian::native>(one32) == one32);
-    CHECK(toEndian<std::endian::native>(one64) == one64);
+    REQUIRE(toEndian<std::endian::native>(one8) == one8);
+    REQUIRE(toEndian<std::endian::native>(one16) == one16);
+    REQUIRE(toEndian<std::endian::native>(one32) == one32);
+    REQUIRE(toEndian<std::endian::native>(one64) == one64);
 
     if constexpr (std::endian::native == std::endian::little) {
-      CHECK(toEndian<std::endian::little>(one8) == one8);
-      CHECK(toEndian<std::endian::little>(one16) == one16);
-      CHECK(toEndian<std::endian::little>(one32) == one32);
-      CHECK(toEndian<std::endian::little>(one64) == one64);
-      CHECK(toEndian<std::endian::big>(one8) == one8);
-      CHECK(toEndian<std::endian::big>(one16) != one16);
-      CHECK(toEndian<std::endian::big>(one32) != one32);
-      CHECK(toEndian<std::endian::big>(one64) != one64);
-      CHECK(toEndian<std::endian::big>(one8) == inv8);
-      CHECK(toEndian<std::endian::big>(one16) == inv16);
-      CHECK(toEndian<std::endian::big>(one32) == inv32);
-      CHECK(toEndian<std::endian::big>(one64) == inv64);
+      REQUIRE(toEndian<std::endian::little>(one8) == one8);
+      REQUIRE(toEndian<std::endian::little>(one16) == one16);
+      REQUIRE(toEndian<std::endian::little>(one32) == one32);
+      REQUIRE(toEndian<std::endian::little>(one64) == one64);
+      REQUIRE(toEndian<std::endian::big>(one8) == one8);
+      REQUIRE(toEndian<std::endian::big>(one16) != one16);
+      REQUIRE(toEndian<std::endian::big>(one32) != one32);
+      REQUIRE(toEndian<std::endian::big>(one64) != one64);
+      REQUIRE(toEndian<std::endian::big>(one8) == inv8);
+      REQUIRE(toEndian<std::endian::big>(one16) == inv16);
+      REQUIRE(toEndian<std::endian::big>(one32) == inv32);
+      REQUIRE(toEndian<std::endian::big>(one64) == inv64);
     } else {
-      CHECK(toEndian<std::endian::big>(one8) == one8);
-      CHECK(toEndian<std::endian::big>(one16) == one16);
-      CHECK(toEndian<std::endian::big>(one32) == one32);
-      CHECK(toEndian<std::endian::big>(one64) == one64);
-      CHECK(toEndian<std::endian::little>(one8) == one8);
-      CHECK(toEndian<std::endian::little>(one16) != one16);
-      CHECK(toEndian<std::endian::little>(one32) != one32);
-      CHECK(toEndian<std::endian::little>(one64) != one64);
-      CHECK(toEndian<std::endian::little>(one8) == inv8);
-      CHECK(toEndian<std::endian::little>(one16) == inv16);
-      CHECK(toEndian<std::endian::little>(one32) == inv32);
-      CHECK(toEndian<std::endian::little>(one64) == inv64);
+      REQUIRE(toEndian<std::endian::big>(one8) == one8);
+      REQUIRE(toEndian<std::endian::big>(one16) == one16);
+      REQUIRE(toEndian<std::endian::big>(one32) == one32);
+      REQUIRE(toEndian<std::endian::big>(one64) == one64);
+      REQUIRE(toEndian<std::endian::little>(one8) == one8);
+      REQUIRE(toEndian<std::endian::little>(one16) != one16);
+      REQUIRE(toEndian<std::endian::little>(one32) != one32);
+      REQUIRE(toEndian<std::endian::little>(one64) != one64);
+      REQUIRE(toEndian<std::endian::little>(one8) == inv8);
+      REQUIRE(toEndian<std::endian::little>(one16) == inv16);
+      REQUIRE(toEndian<std::endian::little>(one32) == inv32);
+      REQUIRE(toEndian<std::endian::little>(one64) == inv64);
     }
   }
 
   SECTION("toNative") {
-    CHECK(toNative<std::endian::native>(one8) == one8);
-    CHECK(toNative<std::endian::native>(one16) == one16);
-    CHECK(toNative<std::endian::native>(one32) == one32);
-    CHECK(toNative<std::endian::native>(one64) == one64);
+    REQUIRE(toNative<std::endian::native>(one8) == one8);
+    REQUIRE(toNative<std::endian::native>(one16) == one16);
+    REQUIRE(toNative<std::endian::native>(one32) == one32);
+    REQUIRE(toNative<std::endian::native>(one64) == one64);
 
     if constexpr (std::endian::native == std::endian::little) {
-      CHECK(toNative<std::endian::little>(one8) == one8);
-      CHECK(toNative<std::endian::little>(one16) == one16);
-      CHECK(toNative<std::endian::little>(one32) == one32);
-      CHECK(toNative<std::endian::little>(one64) == one64);
-      CHECK(toNative<std::endian::big>(one8) == one8);
-      CHECK(toNative<std::endian::big>(one16) != one16);
-      CHECK(toNative<std::endian::big>(one32) != one32);
-      CHECK(toNative<std::endian::big>(one64) != one64);
-      CHECK(toNative<std::endian::big>(one8) == inv8);
-      CHECK(toNative<std::endian::big>(one16) == inv16);
-      CHECK(toNative<std::endian::big>(one32) == inv32);
-      CHECK(toNative<std::endian::big>(one64) == inv64);
+      REQUIRE(toNative<std::endian::little>(one8) == one8);
+      REQUIRE(toNative<std::endian::little>(one16) == one16);
+      REQUIRE(toNative<std::endian::little>(one32) == one32);
+      REQUIRE(toNative<std::endian::little>(one64) == one64);
+      REQUIRE(toNative<std::endian::big>(one8) == one8);
+      REQUIRE(toNative<std::endian::big>(one16) != one16);
+      REQUIRE(toNative<std::endian::big>(one32) != one32);
+      REQUIRE(toNative<std::endian::big>(one64) != one64);
+      REQUIRE(toNative<std::endian::big>(one8) == inv8);
+      REQUIRE(toNative<std::endian::big>(one16) == inv16);
+      REQUIRE(toNative<std::endian::big>(one32) == inv32);
+      REQUIRE(toNative<std::endian::big>(one64) == inv64);
     } else {
-      CHECK(toNative<std::endian::big>(one8) == one8);
-      CHECK(toNative<std::endian::big>(one16) == one16);
-      CHECK(toNative<std::endian::big>(one32) == one32);
-      CHECK(toNative<std::endian::big>(one64) == one64);
-      CHECK(toNative<std::endian::little>(one8) == one8);
-      CHECK(toNative<std::endian::little>(one16) != one16);
-      CHECK(toNative<std::endian::little>(one32) != one32);
-      CHECK(toNative<std::endian::little>(one64) != one64);
-      CHECK(toNative<std::endian::little>(one8) == inv8);
-      CHECK(toNative<std::endian::little>(one16) == inv16);
-      CHECK(toNative<std::endian::little>(one32) == inv32);
-      CHECK(toNative<std::endian::little>(one64) == inv64);
+      REQUIRE(toNative<std::endian::big>(one8) == one8);
+      REQUIRE(toNative<std::endian::big>(one16) == one16);
+      REQUIRE(toNative<std::endian::big>(one32) == one32);
+      REQUIRE(toNative<std::endian::big>(one64) == one64);
+      REQUIRE(toNative<std::endian::little>(one8) == one8);
+      REQUIRE(toNative<std::endian::little>(one16) != one16);
+      REQUIRE(toNative<std::endian::little>(one32) != one32);
+      REQUIRE(toNative<std::endian::little>(one64) != one64);
+      REQUIRE(toNative<std::endian::little>(one8) == inv8);
+      REQUIRE(toNative<std::endian::little>(one16) == inv16);
+      REQUIRE(toNative<std::endian::little>(one32) == inv32);
+      REQUIRE(toNative<std::endian::little>(one64) == inv64);
     }
   }
 
   SECTION("toEndian -> toNative") {
-    CHECK(toNative<std::endian::little>(toEndian<std::endian::little>(one8)) == one8);
-    CHECK(toNative<std::endian::little>(toEndian<std::endian::little>(one16)) == one16);
-    CHECK(toNative<std::endian::little>(toEndian<std::endian::little>(one32)) == one32);
-    CHECK(toNative<std::endian::little>(toEndian<std::endian::little>(one64)) == one64);
-    CHECK(toNative<std::endian::big>(toEndian<std::endian::big>(one8)) == one8);
-    CHECK(toNative<std::endian::big>(toEndian<std::endian::big>(one16)) == one16);
-    CHECK(toNative<std::endian::big>(toEndian<std::endian::big>(one32)) == one32);
-    CHECK(toNative<std::endian::big>(toEndian<std::endian::big>(one64)) == one64);
+    REQUIRE(toNative<std::endian::little>(toEndian<std::endian::little>(one8)) == one8);
+    REQUIRE(toNative<std::endian::little>(toEndian<std::endian::little>(one16)) == one16);
+    REQUIRE(toNative<std::endian::little>(toEndian<std::endian::little>(one32)) == one32);
+    REQUIRE(toNative<std::endian::little>(toEndian<std::endian::little>(one64)) == one64);
+    REQUIRE(toNative<std::endian::big>(toEndian<std::endian::big>(one8)) == one8);
+    REQUIRE(toNative<std::endian::big>(toEndian<std::endian::big>(one16)) == one16);
+    REQUIRE(toNative<std::endian::big>(toEndian<std::endian::big>(one32)) == one32);
+    REQUIRE(toNative<std::endian::big>(toEndian<std::endian::big>(one64)) == one64);
   }
 }
